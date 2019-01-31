7143ed9261113654a967526ed463aeee5334b236
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-31 21:05:22 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 増田弘毅 on 2019/01/31.
+//  Copyright © 2019 rebirthbtn24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
